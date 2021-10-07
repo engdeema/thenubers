d18e4b45b64266f339c1b9746d0288c5b067a581
Deema Burhama <eng.dburhama@gmail.com>
2021-10-08 00:55:39 +0300

    aftercss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
 import "./App.css";
 import React, { useState } from "react";
+import imge from "./pic/immgg.jpg";
+import signature from "./pic/sig.gif";
+import Discreption from "./components/Discreption";
 const random = Math.floor(Math.random() * 10);
+
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const [numberOfGuess, setNumberOfGuess] = useState(3);
+  const [msg, setMsg] = useState("welcome");
+  let array = [random - 1, random - 2, random + 1, random + 2];
+
   function guessNumber() {
-    if (randomNumber !== random) {
-      console.log("you are wrong");
+    // console.log(random);
+    if (numberOfGuess === 0) {
+      setMsg("No more attepts");
     } else {
-      console.log("You are wright");
+      if (+userInput !== +random) {
+        if (array.includes(+userInput)) {
+          setMsg("You are close!");
+        } else {
+          setMsg("You are Faaar!");
+        }
+      } else {
+        setMsg("You are wright");
+      }
     }
+
+    setNumberOfGuess(numberOfGuess - 1);
+    console.log(numberOfGuess);
   }
-  const [randomNumber, setrandomNumber] = useState("");
+
+  function restart() {
+    setNumberOfGuess(3);
+    window.location.reload();
+  }
 
   return (
-    <div className="App">
-      <h5 className="title">Choose WISELY !!</h5>
+    <div>
+      <div className="App">
+        <h5 className="title"></h5>
+        <p> {random}</p>
+        <h1> {msg} </h1>
 
-      <p> {random} </p>
-      <input onChange={(e) => setrandomNumber(e.target.value)} />
+        <input
+          class="txt "
+          type="text"
+          name="choose"
+          placeholder="Choose wisely"
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+        {/* if statement for the button*/}
+        {numberOfGuess === 0 || msg === "You are wright" ? (
+          <button class="butt" onClick={restart}>
+            Restart the Game
+          </button>
+        ) : (
+          <button class="butt" onClick={guessNumber}>
+            Try it your self
+          </button>
+        )}
+        {/* <button onClick={()=>guessNumber()}>Try it your self</button> */}
+      </div>
+      <div className="discibe">
+        <Discreption />
+      </div>
 
-      <button onClick={guessNumber}>Try it your self</button>
+      <img className="sig-detail" src={signature} />
     </div>
   );
 }
---
+++ src/components/Discreption.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Discreption() {
+  return (
+    <div>
+      <p>
+        This Game is made for you , <br /> just have fun , <br />
+        play with the kids , <br /> and save the best Memories with them.
+      </p>
+    </div>
+  );
+}
